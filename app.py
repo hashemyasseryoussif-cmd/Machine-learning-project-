3fed5f9446f7b7860376979dab0f6d8a6d5f24eb
hashemyasseryoussif-cmd <hashemyasseryoussif@gmail.com>
2026-05-30 14:14:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.write(
 
 @st.cache_resource
 def load_model():
-    model = joblib.load("breast_cancer_model (1).pkl")
+    model = joblib.load("breast_cancer_model.pkl")
     return model
 
 try:
